3f014ac95a191effaa88d273f28e77bc923fdd2d
Richard Marcell <richard.marcell8888@gmail.com>
2023-06-23 11:37:44 +0700

    menghapus kembali resource yang tidak diperlukan

--- libs/components/PokemonFilter/Untitled-1.ts
+++
@@ -1,7 +0,0 @@
-"http://localhost:3000/pokemon?
-pokemonName=ha&
-pokemonGender=Mayoritas%20Male&
-pokemonClassification=Seed%20Pok%C3%A9mon,Flame%20Pok%C3%A9mon&
-pokemonType=fire,water&
-height=%3C10,=15&
-weight==12,%3C20";
